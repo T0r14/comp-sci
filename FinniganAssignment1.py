ea0d88dae9e40f8b7666a694891eab4e629c3e0a
T0r14 <99436407+T0r14@users.noreply.github.com>
2022-02-10 14:55:16 -0500

    Create FinniganAssignment1.py
    Surface area assignment

---
+++ FinniganAssignment1.py
@@ -0,0 +1,12 @@
+FinniganAssignment1.py
+#Victoria Finnigan
+# Feb 9, 2022
+
+#create variable of type integer called side length get user to "Enter side length" into side length.
+Side_length = int(input("Enter Side Lenghth"))
+
+#create variable of type integer called "Surface Area" and set it equal to 5*(3**0.5)*((Side _length)**2)
+Surface_Area = 5*(3**0.5)*((Side_length)**2)
+
+#output surface area to user
+print(Surface_Area)
